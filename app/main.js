944be7377616a17c8eb920af8b91b3da6c266cd9
Igal Alkon <igal.alkon@gmail.com>
2017-03-22 07:58:02 +0200

    So ... ammonext physics is a go!

--- app/main.js
+++ app/main.js
@@ -5,17 +5,26 @@ import {Sphere} from '@whs+meshes/Sphere';
 // Physics
 import {WorldModule} from '../node_modules/physics-module-ammonext/src/modules/WorldModule';
 import {SphereModule} from '../node_modules/physics-module-ammonext/src/modules/SphereModule';
+import {PlaneModule} from '../node_modules/physics-module-ammonext/src/modules/PlaneModule';
 
-// THREE
-import {MeshBasicMaterial} from '../node_modules/three/src/materials/MeshBasicMaterial.js';
-
-// Whitestorm
+// WHS Core Modules
 import {
   ElementModule,
   SceneModule,
   CameraModule,
   RenderingModule
 } from '@whs:app';
+
+// WHS Lights
+import {
+    PointLight,
+    AmbientLight
+} from '@whs+lights';
+
+// WHS Meshes
+import {
+    Plane
+} from '@whs+meshes';
 
 // Controls
 import {OrbitModule} from '@whs:controls/orbit';
@@ -24,7 +33,6 @@ import {OrbitModule} from '@whs:controls/orbit';
 import {FancyMaterialModule} from './modules/FancyMaterialModule';
 
 // Components
-import {Plane} from '@whs+meshes/Plane';
 import {BasicComponent} from './components/BasicComponent';
 
 // Game Instance
@@ -33,36 +41,81 @@ const game = new App([
         ammo: 'http://localhost:8080/node_modules/three/examples/js/libs/ammo.js'
     }),
     new ElementModule({
-    container: document.getElementById('app')
+        container: document.getElementById('app')
     }),
     new SceneModule(),
     new CameraModule({
-    position: {
-      z: -15
-    }
+        position: {
+          y: 60,
+          z: 60,
+        }
     }),
     new RenderingModule({bgColor: 0x000001}),
-new OrbitModule()
+    new OrbitModule()
 ]);
 
-game.add(new BasicComponent({
-  modules: [
-    new FancyMaterialModule(game)
-  ]
-}));
+new BasicComponent({
+    modules: [
+        new FancyMaterialModule(game)
+    ]
+}).addTo(game);
 
-game.add(new Sphere({
+// Sphere + SphereModule
+new Sphere({
     geometry: {
         radius: 3
     },
-
+    position: {
+        y: 70
+    },
     modules: [
         new SphereModule({
-            mass: 10
+            mass: 10,
+            restitution: 2.5
         })
     ],
 
-    material: new MeshBasicMaterial({color: 0xff0000}) // red material
-}));
+    material: new THREE.MeshBasicMaterial({color: 0xff0000}) // red material
+}).addTo(game);
+
+// Plane
+new Plane({
+    geometry: {
+        width:  100,
+        height: 100
+    },
+
+    modules: [
+        new PlaneModule({
+            mass: 0
+        })
+    ],
+
+    material: new THREE.MeshPhongMaterial({color: 0x447F8B}),
+
+    rotation: {
+        x: -Math.PI / 2
+    }
+}).addTo(game);
+
+// Lights
+new PointLight({
+    light: {
+        intensity: 0.5,
+        distance: 100
+    },
+
+    shadow: {
+        fov: 90
+    },
+
+    position: new THREE.Vector3(0, 10, 10)
+}).addTo(game);
+
+new AmbientLight({
+    light: {
+        intensity: 0.4
+    }
+}).addTo(game);
 
 game.start();
